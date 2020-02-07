452d27c5dce7e1b99f070ea0cbc95d23cb3d5f85
Cindy Hernandez <cindy-marie@optimum.net>
2020-02-07 08:40:16 -0500

    init sign in function
    - added init sign in function
    - added call to init function in html

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
  function onSignIn(googleUser) {
         // Useful data for your client-side scripts:
+        console.log("this ran");
         var profile = googleUser.getBasicProfile();
         console.log("ID: " + profile.getId()); // Don't send this directly to your server!
         console.log('Full Name: ' + profile.getName());
@@ -12,4 +13,10 @@
         var id_token = googleUser.getAuthResponse().id_token;
         console.log("ID Token: " + id_token);
         window.location.href = "profile.html";
-      }
+}
+
+function init() {
+  gapi.load('auth2', function() {
+    /* Ready. Make a call to gapi.auth2.init or some other API */
+  });
+}
